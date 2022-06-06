6dd9dfb78e3c1cbdbe4e5dfcae34c198e0811af2
prashantcn <prashantcn13@gmail.com>
2022-06-06 12:41:09 +0530

    to maven
    converted to maven

--- java/Prime.java
+++
@@ -1,9 +0,0 @@
-class Prime
-{
-	public static void main(String a[])
-{
-	int i,j;
-
-
-}
-}
---
+++ src/main/java/com/empdemo/Test.java
@@ -0,0 +1,11 @@
+package com.empdemo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("First Git Hub Project Migration");
+	}
+
+}
